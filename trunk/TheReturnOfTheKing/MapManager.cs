75e91bdf0543f3364b8dd90610a35c9eadd8a1c0
ngdacphu.khtn@gmail.com <ngdacphu.khtn@gmail.com@1d5bb684-97f2-abdb-57d1-429f95dab8a9>
2011-05-15 17:59:22 +0000

    Xu ly qua map don gian

--- trunk/TheReturnOfTheKing/MapManager.cs
+++ trunk/TheReturnOfTheKing/MapManager.cs
@@ -39,67 +39,60 @@ namespace TheReturnOfTheKing
         }
         public override bool InitOne(ContentManager content, int id)
         {
-            try
+            XmlDocument _doc = new XmlDocument();
+            _doc.Load(_xmlInfo);
+            XmlNode _map = _doc.SelectSingleNode(@"//Map[@id = '" + id.ToString() + "']");
+
+            _prototype[id] = new Map();
+            int _cols = int.Parse(_map.SelectSingleNode("Width").InnerText);
+            int _rows = int.Parse(_map.SelectSingleNode("Height").InnerText);
+            int _pieceWidth = int.Parse(_map.SelectSingleNode("PieceWidth").InnerText);
+            int _pieceHeight = int.Parse(_map.SelectSingleNode("PieceHeight").InnerText);
+            int _npiece = _prototype[id]._nsprite = _cols * _rows;
+            _prototype[id]._sprite = new GameSprite[_npiece];
+            string contentName = _map.SelectSingleNode("ContentName").InnerText;
+            for (int i = 0; i < _npiece; ++i)
             {
-                XmlDocument _doc = new XmlDocument();
-                _doc.Load(_xmlInfo);
-                XmlNode _map = _doc.SelectSingleNode(@"//Map[@id = '" + id.ToString() + "']");
-
-                _prototype[id] = new Map();                
-                int _cols = int.Parse(_map.SelectSingleNode("//Width").InnerText);
-                int _rows = int.Parse(_map.SelectSingleNode("//Height").InnerText);
-                int _pieceWidth = int.Parse(_map.SelectSingleNode("//PieceWidth").InnerText);
-                int _pieceHeight = int.Parse(_map.SelectSingleNode("//PieceHeight").InnerText);
-                int _npiece = _prototype[id]._nsprite = _cols * _rows;
-                _prototype[id]._sprite = new GameSprite[_npiece];
-                string contentName = _map.SelectSingleNode("//ContentName").InnerText;
-                for (int i = 0; i < _npiece; ++i)
-                {
-                    _prototype[id]._sprite[i] = new GameSprite(content.Load<Texture2D>(contentName + i.ToString("0000")), (i % _cols) * _pieceWidth, (i / _cols) * _pieceHeight);
-                }
-                ((Map)_prototype[id]).Width = _prototype[id]._sprite[0].Texture2D[0].Width * _cols;
-                ((Map)_prototype[id]).Height = _prototype[id]._sprite[0].Texture2D[0].Height * _rows;
-                ((Map)_prototype[id]).Cols = _cols;
-                ((Map)_prototype[id]).Rows = _rows;
-                ((Map)_prototype[id]).RpF = GlobalVariables.ScreenHeight / _pieceHeight + 1;
-                ((Map)_prototype[id]).CpF = GlobalVariables.ScreenWidth / _pieceWidth + 1;
-                ((Map)_prototype[id]).StartPointX = int.Parse(_map.SelectSingleNode("//StartPointX").InnerText);
-                ((Map)_prototype[id]).StartPointY = int.Parse(_map.SelectSingleNode("//StartPointY").InnerText);
-
-                string collisionName = _map.SelectSingleNode("//Collision").InnerText;
-                List<List<bool>> matrix = new List<List<bool>>();
-                int collisionUnitDim = int.Parse(_map.SelectSingleNode("//CollisionUnitDim").InnerText);
-                int collisionMatrixWith = (int)((Map)_prototype[id]).Width / collisionUnitDim;
-                int collisionMatrixHeight = (int)((Map)_prototype[id]).Height / collisionUnitDim;
-                FileStream f = File.OpenRead(collisionName);
-
-                List<bool> temp = new List<bool>();
-                while (true)
-                {
-                    int i = f.ReadByte();
-                    if (i == -1)
-                        break;
-                    if (i == '\r' || i == ' ' || i == '\n')
-                        continue;
-                    if (i == '1')
-                        temp.Add(true);
-                    else
-                        temp.Add(false);
-                    if (temp.Count == collisionMatrixWith)
-                    {
-                        matrix.Add(temp);
-                        temp = new List<bool>();
-                    }
-                }
-                ((Map)_prototype[id]).Matrix = matrix;
-                ((Map)_prototype[id]).CollisionDim = collisionUnitDim;
-                
-                return true;
+                _prototype[id]._sprite[i] = new GameSprite(content.Load<Texture2D>(contentName + i.ToString("0000")), (i % _cols) * _pieceWidth, (i / _cols) * _pieceHeight);
             }
-            catch
+            ((Map)_prototype[id]).Width = _prototype[id]._sprite[0].Texture2D[0].Width * _cols;
+            ((Map)_prototype[id]).Height = _prototype[id]._sprite[0].Texture2D[0].Height * _rows;
+            ((Map)_prototype[id]).Cols = _cols;
+            ((Map)_prototype[id]).Rows = _rows;
+            ((Map)_prototype[id]).RpF = GlobalVariables.ScreenHeight / _pieceHeight + 1;
+            ((Map)_prototype[id]).CpF = GlobalVariables.ScreenWidth / _pieceWidth + 1;
+            ((Map)_prototype[id]).StartPointX = int.Parse(_map.SelectSingleNode("StartPointX").InnerText);
+            ((Map)_prototype[id]).StartPointY = int.Parse(_map.SelectSingleNode("StartPointY").InnerText);
+
+            string collisionName = _map.SelectSingleNode("Collision").InnerText;
+            List<List<bool>> matrix = new List<List<bool>>();
+            int collisionUnitDim = int.Parse(_map.SelectSingleNode("CollisionUnitDim").InnerText);
+            int collisionMatrixWith = (int)((Map)_prototype[id]).Width / collisionUnitDim;
+            int collisionMatrixHeight = (int)((Map)_prototype[id]).Height / collisionUnitDim;
+            FileStream f = File.OpenRead(collisionName);
+
+            List<bool> temp = new List<bool>();
+            while (true)
             {
-                return false;
+                int i = f.ReadByte();
+                if (i == -1)
+                    break;
+                if (i == '\r' || i == ' ' || i == '\n')
+                    continue;
+                if (i == '1')
+                    temp.Add(true);
+                else
+                    temp.Add(false);
+                if (temp.Count == collisionMatrixWith)
+                {
+                    matrix.Add(temp);
+                    temp = new List<bool>();
+                }
             }
+            ((Map)_prototype[id]).Matrix = matrix;
+            ((Map)_prototype[id]).CollisionDim = collisionUnitDim;
+            ((Map)_prototype[id]).MapName = _map.SelectSingleNode(@"MapName").InnerText;
+            return true;
         }
     }
 }
